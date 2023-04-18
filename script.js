11dfee033f611bf655e21fba29330d9079f3f5eb
Plasnid <stuart@sackler.ca>
2023-04-18 17:33:00 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+/**
+ * ! Coding for Design Final
+ * ! Total Points: 70
+ */
+
+/**
+* * Section 1: Arrays - 20 points
+ */
+
+/** 
+ * ! Q1: Using Array Co-ordinates, and the array below create 5 words
+ * !  that contain at least 4 letters each.  Console log each word using template literals.
+ * 
+*/
+
+let letters  = [
+    ["a", "b", "c", "d", "e", "f"],
+    ["g", "h", "i", "j", "k", "l"],
+    ["m", "n", "o", "p", "q", "r"],
+    ["s", "t", "u", "v", "w", "x"],
+    ["y", "z"]
+]
+
+/**
+ * * Section 2: Functions - 10 points
+ */
+
+/**
+ * ! Q2: Create a fat arrow function that creates an h1 tag on the screen.  
+ * !      Make the text of that h1 tag your name and your student # using javascript.
+ * !      Write a class in the css file that changes the border to a colour of your choice
+ * !      Use Javascript to add that class to the h1 tag
+ * !      Use Javascript(no libraries) to add a click event that console logs the same text as on the h1 tag
+ */
+
+
+/**
+ * * Section 3: Looping - 10 points
+ */
+
+/**
+ * ! Q3:  Take the sentence below, and using the split command(eg. stringName.split(" ")), transform
+ * ! it into an array, then use an for loop running in reverse to write it in P tags on the screen
+ */
+
+let sentence  = "much! very very so it love I Gosh ride. carnival a than fun more is Coding";
+
+
+/**
+ * * Section 4: DOM Manipulation - 30 points
+ */
+/**
+ * ! Use DOM Manipulation, CSS Classes, classList, and a function declaration.
+ * ! Make one of the following out of html tags, created, styled, and placed on the screen using javascript
+ * !    -smiley face(with circular eyes)
+ * !    -sad face(also with circular eyes)
+ * !    -winking face(with at least one circular eye)
+ * !    -sleeping face
+ * 
+ * !  Use at least 6 shapes for your selection.  Be sure to make use of width, height, position, backgound colour
+ * !  rotation, display, and border
+ * 
+ * !  All styles created must be classes.  Classes may only be added used DOM Manipulation.  Put all instructions
+ * !  for making your shape in a function declaration.  Run your function to show the creation on screen.
+ */
